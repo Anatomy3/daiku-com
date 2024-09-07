636439bf4ca0cfa3b7f6eea33dc5358244b7fa60
Vigio <linggacrack@gmail.com>
2024-09-07 10:17:03 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import HomePage from '../components/HomePage';
-import About from '../components/About';  // Mengimpor komponen About
+import About from '../components/About';
 import Projects from '../components/Projects';
 import Footer from '../components/Footer';
 import Contact from '../components/Contact';
 import Sidebar from '../components/Sidebar';
+import Artikel from '../components/Artikel'; // Import Komponen Artikel
 
 export default function Home() {
   const [isSidebarExpanded, setIsSidebarExpanded] = useState(false);
+  
+  // Gunakan useRef untuk merujuk pada section Projects dan Artikel
+  const projectsSectionRef = useRef(null);
+  const artikelSectionRef = useRef(null); // Refs untuk Artikel
+
+  // Fungsi untuk scroll ke bagian yang diinginkan
+  const scrollToSection = (section) => {
+    if (section === 'projectsSection') {
+      projectsSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    } else if (section === 'artikelSection') { // Tambahkan bagian untuk scroll ke Artikel
+      artikelSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   const toggleSidebar = () => {
     setIsSidebarExpanded(!isSidebarExpanded);
@@ -16,10 +30,10 @@ export default function Home() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', overflowX: 'hidden' }}>
       <div style={{ display: 'flex', flex: 1, overflowX: 'hidden' }}>
-        <Sidebar isManualExpanded={isSidebarExpanded} />
+        {/* Pass scrollToSection function ke Sidebar */}
+        <Sidebar scrollToSection={scrollToSection} isManualExpanded={isSidebarExpanded} />
         <div className="content-wrapper">
           <style jsx global>{`
-            /* Mengatur scrollbar kustom untuk WebKit browsers (Chrome, Safari, Edge) */
             ::-webkit-scrollbar {
               width: 12px;
               height: 12px;
@@ -32,14 +46,14 @@ export default function Home() {
             }
 
             ::-webkit-scrollbar-thumb {
-              background: orange; /* Ubah warna menjadi oranye */
+              background: orange;
               backdrop-filter: blur(10px);
               border-radius: 9999px;
               border: 2px solid rgba(255, 255, 255, 0.2);
             }
 
             ::-webkit-scrollbar-thumb:hover {
-              background: #e69500; /* Warna saat hover, sedikit lebih gelap dari oranye */
+              background: #e69500;
             }
 
             ::-webkit-scrollbar-corner {
@@ -51,37 +65,44 @@ export default function Home() {
             /* Untuk Firefox */
             * {
               scrollbar-width: thin;
-              scrollbar-color: orange rgba(255, 255, 255, 0.2); /* Ubah warna menjadi oranye */
+              scrollbar-color: orange rgba(255, 255, 255, 0.2);
             }
 
-            /* Gaya untuk mengatur keseluruhan tampilan */
             body {
               margin: 0;
               padding: 0;
               overflow-x: hidden;
             }
 
-            /* Media query untuk layar kecil atau mode mobile */
             @media (max-width: 768px) {
               .content-wrapper {
                 margin-left: 0 !important;
-                padding-top: 0; /* Hapus padding atas */
+                padding-top: 0;
               }
             }
 
-            /* Default layout untuk desktop */
             .content-wrapper {
               flex: 1;
               margin-left: 58px;
-              padding-top: 0; /* Hapus padding atas */
+              padding-top: 0;
               overflow-x: hidden;
             }
           `}</style>
-          <HomePage /> {/* Mengganti Header dengan HomePage */}
-          <About /> {/* Tambahkan halaman About di sini */}
-          <Projects /> {/* Mengganti Gallery dengan Projects */}
-          <Contact />
-          <Footer />
+          <HomePage /> {/* Halaman HomePage */}
+          <About /> {/* Halaman About */}
+          
+          {/* Tambahkan ref ke bagian Projects */}
+          <div ref={projectsSectionRef}>
+            <Projects /> {/* Section Projects */}
+          </div>
+          
+          {/* Tambahkan ref ke bagian Artikel */}
+          <div ref={artikelSectionRef}>
+            <Artikel /> {/* Section Artikel */}
+          </div>
+
+          <Contact /> {/* Halaman Contact */}
+          <Footer /> {/* Footer */}
         </div>
       </div>
     </div>
